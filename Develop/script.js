36c7264e6d77c7159a47173e173ba260781a62a3
Thor138 <tjcourey84@gmail.com>
2021-04-22 16:40:10 -0600

    added checks to length, and characters. Added functionality to clear password every time

--- Develop/script.js
+++ Develop/script.js
@@ -94,8 +94,7 @@ var symbol = [
 ];
 
 var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-//temp var until linked to html
+var password = "";
 var upperCaseSelected = "";
 var lowerCaseSelected = "";
 var symbolSelected = "";
@@ -121,20 +120,29 @@ var arrayFunction = function () {
   return finalArray;
 };
 
-var length = lengthSelected; // could be constant
-
-var password = "";
-
 function randomChar(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function generatePassword() {
   var length = prompt("select between 8-128");
+  if (length < 8 || length > 128) {
+    alert("Please select between 8 and 128 Characters");
+    return;
+  }
   upperCaseSelected = confirm("Do you want to use upper case?");
   lowerCaseSelected = confirm("Do you want to use lower case?");
   symbolSelected = confirm("Do you want to use symbols?");
   numSelected = confirm("Do you want to use numbers?");
+  if (
+    !upperCaseSelected &&
+    !lowerCaseSelected &&
+    !symbolSelected &&
+    !numSelected
+  ) {
+    alert("Please choose at least one option");
+    return;
+  }
   arrayFunction();
   for (i = 0; i <= length; i++) {
     password = password.concat(randomChar(finalArray));
@@ -143,10 +151,13 @@ function generatePassword() {
 }
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  console.log(password);
   passwordText.value = password;
+  password = "";
+  finalArray = [];
+  console.log(password);
 }
 
 // Add event listener to generate button
